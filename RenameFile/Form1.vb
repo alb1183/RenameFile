cf85ee8359a5fb41d803d12a42f0782792d996f9
Alberto <alber1183@hotmail.com>
2016-03-28 18:32:43 +0200

    Ficheros iniciales

---
+++ RenameFile/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim directorio As IO.FileInfo() = New IO.DirectoryInfo(TextBox1.Text).GetFiles()
+        Dim fichero As IO.FileInfo
+
+        For Each fichero In directorio
+            'MsgBox(fichero.FullName)
+            'MsgBox(fichero.Name)
+            If fichero.Name.Contains(TextBox2.Text) Then
+                My.Computer.FileSystem.RenameFile(fichero.FullName, fichero.Name.Replace(TextBox2.Text, TextBox3.Text))
+            End If
+        Next
+    End Sub
+End Class
